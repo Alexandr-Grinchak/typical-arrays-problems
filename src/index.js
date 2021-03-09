b66f2f7c5e862572821cb6438f31f18cf4c632fa
Alexandr Grinchak <grinchak.alexandr@gmail.com>
2021-03-09 21:06:07 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array && array.length > 0) {
+        let newArray = array.sort((a, b) => a - b);
+        return newArray[0];
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array && array.length > 0) {
+        let newArray = array.sort((a, b) => a - b);
+        return newArray[array.length - 1];
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array && array.length > 0) {
+        let aver = 0;
+    for(let i = 0; i < array.length; i++) {
+        aver = aver + array[i];
+    };
+  return aver / array.length;
+     } else {
+    return 0;
+    }
 }
